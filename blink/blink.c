4233c87edba024c57d980810f66b90c42ce5bbc3
David Goffredo <dmgoffredo@gmail.com>
2024-06-25 21:17:00 -0400

    use GNU screen to monitor output

--- blink/blink.c
+++ blink/blink.c
@@ -4,10 +4,11 @@
 int main() {
     stdio_init_all();
     if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed");
+        printf("Wi-Fi init failed\n");
         return -1;
     }
-    for (;;) {
+    for (int i = 1; ; ++i) {
+        printf("%d blink\n", i);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(250);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
