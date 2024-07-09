81b0d84dc7c8101379555b8aa5b9a64a36e522a4
David Goffredo <dmgoffredo@gmail.com>
2024-07-09 15:16:19 -0400

    coroutines seem to work

--- coroutines/coroutines.cpp
+++ coroutines/coroutines.cpp
@@ -1,28 +1,42 @@
-// #include "hardware/i2c.h"
 #include "pico/async_context_poll.h"
 #include "pico/binary_info.h"
 #include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
 #include <tusb.h>
 
-// #include "scd4x_i2c.h"
 #include <stdio.h>
 #include <chrono>
-#include <coroutine>
+
+#include "picoro.h"
 
 #define CHECKPOINT() printf("[%s:%d]\n", __FILE__, __LINE__)
 
 // Wait for the host to attach to the USB terminal (i.e. ttyACM0).
 // Blink the onboard LED while we're waiting.
 // Give up after the specified number of seconds.
-void wait_for_usb_debug_attach(std::chrono::seconds timeout) {
+picoro::Coroutine wait_for_usb_debug_attach(async_context_t *context, std::chrono::seconds timeout) {
     const int iterations = timeout / std::chrono::seconds(1);
     for (int i = 0; i < iterations && !tud_cdc_connected(); ++i) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(500);
+        co_await picoro::sleep_for(context, std::chrono::milliseconds(500));
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(500);
+        co_await picoro::sleep_for(context, std::chrono::milliseconds(500));
     }
+
+    co_await picoro::sleep_for(context, std::chrono::seconds(1));
+    printf("Glad you could make it.\n");
+}
+
+picoro::Coroutine do_loopy_thing(async_context_t *context) {
+    for (int i = 0; ; ++i) {
+        printf("coroutine tick %d\n", i);
+        co_await picoro::sleep_for(context, std::chrono::seconds(2));
+    }
+}
+
+picoro::Coroutine coroutine_main(async_context_t *context) {
+    co_await wait_for_usb_debug_attach(context, std::chrono::seconds(10));
+    co_await do_loopy_thing(context);
 }
 
 int main() {
@@ -32,8 +46,6 @@ int main() {
         return -1;
     }
 
-    wait_for_usb_debug_attach(std::chrono::seconds(10));
-    
     async_context_poll_t context = {};
     bool succeeded = async_context_poll_init_with_defaults(&context);
     if (!succeeded) {
@@ -41,13 +53,8 @@ int main() {
         return -2;
     }
 
-    // TODO
-    for (;;) {
-        printf("Performing any work enqueued onto the async_context_t\n");
-        async_context_poll(&context.core);
-        printf("Waiting up to one second for work to appear on the async_context_t\n");
-        async_context_wait_for_work_ms(&context.core, 1000);
-    }
+    run_event_loop(&context.core, coroutine_main(&context.core));
 
+    // unreachable
     async_context_deinit(&context.core);
 }
--- coroutines/picoro.h
+++ coroutines/picoro.h
@@ -1,12 +1,17 @@
 #pragma once
 
+#include <chrono>
 #include <coroutine>
+#include <cstdlib>
+#include <memory>
+#include <utility>
+
 #include "pico/async_context.h"
 
 namespace picoro {
 //              _.---..._     
 //           ./^         ^-._       
-//         ./^C===.         ^\.   /\
+//         ./^C===.         ^\.   /\.
 //        .|'     \\        _ ^|.^.|
 //   ___.--'_     ( )  .      ./ /||
 //  /.---^T\      ,     |     / /|||
@@ -25,5 +30,249 @@ namespace picoro {
 // ##########b._     |        _.d#############              
 //
 //                     --- Steven J. Simmons
+
+struct Sleep {
+  async_context_t *context;
+  absolute_time_t deadline;
+};
+
+Sleep sleep_for(async_context_t *context, std::chrono::microseconds delay);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class ScheduledContinuation {
+  async_at_time_worker_t worker_;
+  std::coroutine_handle<> continuation_;
+
+  static void invoke_and_destroy(async_context_t*, async_at_time_worker_t*);
+
+ public:
+  static void create_and_schedule(async_context_t *context, std::coroutine_handle<> continuation, absolute_time_t deadline);
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class Coroutine {
+ public:
+  class Awaiter;
+  class FinalAwaitable;
+  class Promise;
+  struct Deleter {
+    void operator()(Coroutine::Promise *promise) {
+      std::coroutine_handle<Promise>::from_promise(*promise).destroy();
+    }
+  };
+
+  using promise_type = Promise; // required by the C++ coroutine protocol
+  using UniqueHandle= std::unique_ptr<Promise, Deleter>;
+
+ private:
+  UniqueHandle promise_;
+  
+ public:
+  explicit Coroutine(UniqueHandle promise);
+
+  Coroutine(Coroutine&&) = default;
+
+  Coroutine() = delete;
+  Coroutine(const Coroutine&) = delete;
+
+  Awaiter operator co_await();
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+template <typename... Coroutines>
+void run_event_loop(async_context_t *context, Coroutines... toplevel_coroutines);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class Coroutine::FinalAwaitable {
+  std::coroutine_handle<> coroutine_;
+
+ public:
+  explicit FinalAwaitable(std::coroutine_handle<> coroutine);
+
+  bool await_ready() noexcept;
+  std::coroutine_handle<> await_suspend(std::coroutine_handle<>) noexcept;
+  void await_resume() noexcept;
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class Coroutine::Promise {
+  // `continuation_` is what runs after we're done.
+  // By default, it's a no-op handle, which means "nothing to do after."
+  // Sometimes, `Coroutine::Awaiter::await_suspend` will assign a non-no-op
+  // handle to `continuation_`.
+  // `continuation_` is passed to `FinalAwaitable` in `final_suspend`.
+  // `FinalAwaitable` will then return it in `FinalAwaitable::await_suspend`,
+  // which will cause it to be `.resume()`d.
+  std::coroutine_handle<> continuation_;
+
+  friend class Awaiter;
+
+ public:
+  Promise();
+
+  Coroutine get_return_object();
+  std::suspend_never initial_suspend();
+  FinalAwaitable final_suspend() noexcept;
+  void return_void();
+
+  // If we `co_await` a `Sleep`, schedule ourselves as a continuation for when
+  // the sleep is done.
+  std::suspend_always await_transform(Sleep sleep);
+
+  // If we `co_await` a something else, just use the value as-is.
+  template <typename Object>
+  auto &&await_transform(Object &&) const noexcept;
+
+  void unhandled_exception();
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+class Coroutine::Awaiter {
+  Promise *promise_;
+
+ public:
+  explicit Awaiter(Promise *promise);
+
+  bool await_ready();
+  bool await_suspend(std::coroutine_handle<> continuation);
+  void await_resume();
+};
+
+// Implementations
+// ===============
+
+// struct Sleep
+// ------------
+inline
+Sleep sleep_for(async_context_t *context, std::chrono::microseconds delay) {
+  const uint64_t delay_us = delay / std::chrono::microseconds(1);
+  return Sleep{context, make_timeout_time_us(delay_us)};
+}
+
+// class ScheduledContinuation
+// ---------------------------
+inline
+void ScheduledContinuation::invoke_and_destroy(async_context_t*, async_at_time_worker_t* worker) {
+  auto *scheduled = static_cast<ScheduledContinuation*>(worker->user_data);
+  std::coroutine_handle<> handle = scheduled->continuation_;
+  delete scheduled;
+  handle.resume();
+}
+
+inline
+void ScheduledContinuation::create_and_schedule(async_context_t *context, std::coroutine_handle<> continuation, absolute_time_t deadline) {
+  auto *scheduled = new ScheduledContinuation;
+  scheduled->continuation_ = continuation;
+  scheduled->worker_.do_work = &ScheduledContinuation::invoke_and_destroy;
+  scheduled->worker_.user_data = scheduled;
+  const bool added = async_context_add_at_time_worker_at(context, &scheduled->worker_, deadline);
+  (void)added;
+  assert(added);
+}
+
+// class Coroutine
+// ---------------
+inline
+Coroutine::Coroutine(UniqueHandle promise)
+: promise_(std::move(promise)) {}
+
+inline
+Coroutine::Awaiter Coroutine::operator co_await() {
+  return Coroutine::Awaiter(promise_.get());
+}
+
+// class Coroutine::FinalAwaitable
+// -------------------------------
+inline
+Coroutine::FinalAwaitable::FinalAwaitable(std::coroutine_handle<> coroutine)
+: coroutine_(coroutine) {}
+
+inline
+bool Coroutine::FinalAwaitable::await_ready() noexcept { return false; }
+
+inline
+std::coroutine_handle<> Coroutine::FinalAwaitable::await_suspend(std::coroutine_handle<>) noexcept {
+  return coroutine_;
+}
+
+inline
+void Coroutine::FinalAwaitable::await_resume() noexcept {}
+
+// class Coroutine::Promise
+// ------------------------
+inline
+Coroutine::Promise::Promise()
+: continuation_(std::noop_coroutine())
+{}
+
+inline
+Coroutine Coroutine::Promise::get_return_object() {
+  return Coroutine(UniqueHandle(this));
+}
+
+inline
+std::suspend_never Coroutine::Promise::initial_suspend() {
+  return std::suspend_never();
+}
+
+inline
+Coroutine::FinalAwaitable Coroutine::Promise::final_suspend() noexcept {
+  return FinalAwaitable(continuation_);
+}
+
+inline
+void Coroutine::Promise::return_void() {}
+
+inline
+std::suspend_always Coroutine::Promise::await_transform(Sleep sleep) {
+  ScheduledContinuation::create_and_schedule(sleep.context, std::coroutine_handle<Promise>::from_promise(*this), sleep.deadline);
+  return std::suspend_always();
+}
+
+template <typename Object>
+auto&& Coroutine::Promise::await_transform(Object &&object) const noexcept {
+  return std::forward<Object>(object);
+}
+
+inline
+void Coroutine::Promise::unhandled_exception() {
+  std::terminate();
+}
+
+// class Coroutine::Awaiter
+// ------------------------
+inline
+Coroutine::Awaiter::Awaiter(Promise *promise)
+: promise_(promise) {}
+
+inline
+bool Coroutine::Awaiter::await_ready() {
+  return false;
+}
+
+inline
+bool Coroutine::Awaiter::await_suspend(std::coroutine_handle<> continuation) {
+  promise_->continuation_ = continuation;
+  return true;
+}
+
+inline
+void Coroutine::Awaiter::await_resume() {}
+
+// void run_event_loop
+// -------------------
+template <typename... Coroutines>
+void run_event_loop(async_context_t *context, Coroutines... toplevel_coroutines) {
+  for (;;) {
+    async_context_poll(context);
+    async_context_wait_for_work_ms(context, 10 * 1000);
+  }
+}
 
 }  // namespace picoro
