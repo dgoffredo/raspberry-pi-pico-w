79e857c67548f98162bc497d3b951fac90858c98
David Goffredo <dmgoffredo@gmail.com>
2024-07-11 18:17:17 -0400

    HTTP server works :D

--- server/server.cpp
+++ server/server.cpp
@@ -1,13 +1,6 @@
-/**
- * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include <tusb.h>
 
@@ -20,181 +13,197 @@
 
 #include "secrets.h"
 
-#define DEBUG_printf(...) printf(__VA_ARGS__)
+#include <array>
+#include <cmath>
+#include <cstdio>
 
-constexpr uint16_t TCP_PORT = 80;
-constexpr long  BUF_SIZE = 2048;
+#define debug(...) printf(__VA_ARGS__)
 
-typedef struct TCP_SERVER_T_ {
-    struct tcp_pcb *server_pcb;
-    struct tcp_pcb *client_pcb;
-    bool complete;
-    uint8_t buffer_sent[BUF_SIZE];
-    uint8_t buffer_recv[BUF_SIZE];
-    int sent_len;
-    int recv_len;
-    int run_count;
-} TCP_SERVER_T;
-
-static TCP_SERVER_T* tcp_server_init() {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)calloc(1, sizeof(TCP_SERVER_T));
-    if (!state) {
-        DEBUG_printf("failed to allocate state\n");
-        return NULL;
+const char *describe(err_t error) {
+    switch (error) {
+    case ERR_OK: return "[ERR_OK] No error, everything OK";
+    case ERR_MEM: return "[ERR_MEM] Out of memory error";
+    case ERR_BUF: return "[ERR_BUF] Buffer error";
+    case ERR_TIMEOUT: return "[ERR_TIMEOUT] Timeout";
+    case ERR_RTE: return "[ERR_RTE] Routing problem";
+    case ERR_INPROGRESS: return "[ERR_INPROGRESS] Operation in progress";
+    case ERR_VAL: return "[ERR_VAL] Illegal value";
+    case ERR_WOULDBLOCK: return "[ERR_WOULDBLOCK] Operation would block";
+    case ERR_USE: return "[ERR_USE] Address in use";
+    case ERR_ALREADY: return "[ERR_ALREADY] Already connecting";
+    case ERR_ISCONN: return "[ERR_ISCONN] Conn already established";
+    case ERR_CONN: return "[ERR_CONN] Not connected";
+    case ERR_IF: return "[ERR_IF] Low-level netif error";
+    case ERR_ABRT: return "[ERR_ABRT] Connection aborted";
+    case ERR_RST: return "[ERR_RST] Connection reset";
+    case ERR_CLSD: return "[ERR_CLSD] Connection closed";
+    case ERR_ARG: return "[ERR_ARG] Illegal argument";
     }
-    return state;
+    return "Unknown error code.";
 }
 
-err_t tcp_server_close(void *arg) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    err_t err = ERR_OK;
-    if (state->client_pcb != NULL) {
-        tcp_arg(state->client_pcb, NULL);
-        tcp_poll(state->client_pcb, NULL, 0);
-        tcp_sent(state->client_pcb, NULL);
-        tcp_recv(state->client_pcb, NULL);
-        tcp_err(state->client_pcb, NULL);
-        err = tcp_close(state->client_pcb);
-        if (err != ERR_OK) {
-            DEBUG_printf("close failed %d, calling abort\n", err);
-            tcp_abort(state->client_pcb);
-            err = ERR_ABRT;
-        }
-        state->client_pcb = NULL;
+struct Measurement {
+    unsigned sequence_number = 0; 
+    uint16_t co2_ppm = 0;
+    int32_t temperature_millicelsius = 0;
+    int32_t relative_humidity_millipercent = 0;
+} latest;
+
+template <std::size_t buffer_size>
+int format_response(
+    std::array<char, buffer_size>& buffer,
+    const Measurement& data) {
+
+    // 225 bytes is the largest the response could ever be. I counted.
+    static_assert(buffer_size >= 225 + 1);
+
+    constexpr char response_format[] =
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"
+        "{\"sequence_number\": %u,"
+        " \"CO2_ppm\": %hu,"
+        " \"temperature_celsius\": %ld.%03ld,"
+        " \"relative_humidity_percent\": %ld.%03ld}";
+    
+    return std::snprintf(
+        buffer.data(),
+        buffer_size,
+        response_format,
+        data.sequence_number,
+        data.co2_ppm,
+        data.temperature_millicelsius / 1000,
+        std::abs(data.temperature_millicelsius) % 1000,
+        data.relative_humidity_millipercent / 1000,
+        std::abs(data.relative_humidity_millipercent) / 1000);
+}
+
+struct Client {
+    std::array<char, 256> response_buffer;
+    int response_length = 0;
+    int response_bytes_acked = 0;
+};
+
+err_t send_response(Client& client, tcp_pcb *client_pcb) {
+    client.response_length = format_response(client.response_buffer, latest);
+    debug("Sending response of length %d\n", client.response_length);
+
+    const u8_t flags = 0;
+    err_t err = tcp_write(client_pcb, client.response_buffer.data(), client.response_length, flags);
+    if (err) {
+        debug("tcp_write error: %s\n", describe(err));
+        return err;
     }
-    if (state->server_pcb) {
-        tcp_arg(state->server_pcb, NULL);
-        tcp_close(state->server_pcb);
-        state->server_pcb = NULL;
+
+    err = tcp_output(client_pcb); 	
+    if (err) {
+        debug("tcp_output error: %s\n", describe(err));
+    }
+
+    return err;
+}
+
+err_t check(err_t err) {
+    if (err) {
+        debug("err: %s\n", describe(err));
     }
     return err;
 }
 
-err_t tcp_server_done(void *arg) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    DEBUG_printf("Server go bye-bye.\n");
-    state->complete = true;
-    return tcp_server_close(arg);
-}
+err_t cleanup_connection(Client *client, tcp_pcb *client_pcb) {
+    tcp_arg(client_pcb, NULL);
+    tcp_sent(client_pcb, NULL);
+    tcp_recv(client_pcb, NULL);
+    tcp_err(client_pcb, NULL);
 
-err_t tcp_server_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    DEBUG_printf("tcp_server_sent %u\n", len);
-    state->sent_len += len;
+    delete client;
 
-    // TODO: detect whether we sent the whole payload
-    // if (state->sent_len >= BUF_SIZE) {
-    // }
-
-    return ERR_OK;
-}
-
-err_t tcp_server_send_data(void *arg, struct tcp_pcb *tpcb)
-{
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    static const char message[] = "Hello, and goodbye!\n";
-    constexpr long MSG_LEN = sizeof(message) - 1;
-    for(int i = 0; i < MSG_LEN; ++i) {
-        state->buffer_sent[i] = message[i];
-    }
-
-    state->sent_len = 0;
-    DEBUG_printf("Writing %ld bytes to client\n", MSG_LEN);
-
-    err_t err = tcp_write(tpcb, state->buffer_sent, MSG_LEN, TCP_WRITE_FLAG_COPY);
-    if (err != ERR_OK) {
-        DEBUG_printf("Failed to write data %d\n", err);
-        return tcp_server_done(arg);
-    }
-    return ERR_OK;
-}
-
-err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    if (!p) {
-        return tcp_server_done(arg);
-    }
-
-    if (p->tot_len > 0) {
-        DEBUG_printf("tcp_server_recv %d/%d err %d\n", p->tot_len, state->recv_len, err);
-        // Consume the buffer.
-        tcp_recved(tpcb, p->tot_len);
-    }
-    pbuf_free(p);
-
-    return ERR_OK;
-}
-
-void tcp_server_err(void *arg, err_t err) {
-    if (err != ERR_ABRT) {
-        DEBUG_printf("tcp_client_err_fn %d\n", err);
-        tcp_server_done(arg);
-    }
-}
-
-err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb, err_t err) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    if (err != ERR_OK || client_pcb == NULL) {
-        DEBUG_printf("Failure in accept\n");
-        tcp_server_done(arg);
-        return ERR_VAL;
-    }
-    DEBUG_printf("Client connected\n");
-
-    state->client_pcb = client_pcb;
-    tcp_arg(client_pcb, state);
-    tcp_sent(client_pcb, tcp_server_sent);
-    tcp_recv(client_pcb, tcp_server_recv);
-    tcp_err(client_pcb, tcp_server_err);
-
-    return tcp_server_send_data(arg, state->client_pcb);
-}
-
-bool tcp_server_open(void *arg) {
-    TCP_SERVER_T *state = (TCP_SERVER_T*)arg;
-    DEBUG_printf("Starting server at %s on port %u\n", ip4addr_ntoa(netif_ip4_addr(netif_list)), TCP_PORT);
-
-    struct tcp_pcb *pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
-    if (!pcb) {
-        DEBUG_printf("failed to create pcb\n");
-        return false;
-    }
-
-    err_t err = tcp_bind(pcb, NULL, TCP_PORT);
+    err_t err = tcp_close(client_pcb);
     if (err) {
-        DEBUG_printf("failed to bind to port %u\n", TCP_PORT);
-        return false;
+        debug("tcp_close error: %s\n", describe(err));
     }
-
-    state->server_pcb = tcp_listen_with_backlog(pcb, 1);
-    if (!state->server_pcb) {
-        DEBUG_printf("failed to listen\n");
-        if (pcb) {
-            tcp_close(pcb);
-        }
-        return false;
-    }
-
-    tcp_arg(state->server_pcb, state);
-    tcp_accept(state->server_pcb, tcp_server_accept);
-
-    return true;
+    return err;
 }
 
-void run_tcp_server_test(async_context_t *context) {
-    TCP_SERVER_T *state = tcp_server_init();
-    if (!state) {
+err_t on_sent(void *arg, tcp_pcb *client_pcb, u16_t len) {
+    auto *client = static_cast<Client*>(arg);
+    client->response_bytes_acked += len;
+    debug("client ACK'd %d/%d bytes of the sent response\n", client->response_bytes_acked, client->response_length);
+    if (client->response_bytes_acked == client->response_length) {
+        debug("Client has received the entire response. Closing connection.\n");
+        return cleanup_connection(client, client_pcb);
+    }
+    return ERR_OK;
+}
+
+err_t on_recv(void *arg, tcp_pcb *client_pcb, pbuf *buf, err_t err) {
+    if (err) {
+        debug("on_recv error: %s\n", describe(err));
+        if (buf) {
+            pbuf_free(buf);
+        }
+        return ERR_OK;
+    }
+
+    if (!buf) {
+        debug("The client closed their side of the connection. Closing our side now.\n");
+        return cleanup_connection(static_cast<Client*>(arg), client_pcb);
+    }
+
+    if (buf->tot_len > 0) {
+        debug("tcp_server_recv %d bytes. err: %d\n", buf->tot_len, err);
+        tcp_recved(client_pcb, buf->tot_len);
+    }
+    pbuf_free(buf);
+
+    return ERR_OK;
+}
+
+void on_err(void *arg, err_t err) {
+    debug("connection fatal error: %s\n", describe(err));
+    delete static_cast<Client*>(arg);
+}
+
+err_t on_accept(void *arg, tcp_pcb *client_pcb, err_t err) {
+    if (err || client_pcb == NULL) {
+        debug("failed to accept a connection: %s\n", describe(err));
+        return ERR_VAL; // Is this the appropriate return value?
+    }
+    debug("client connected\n");
+
+    auto *client = new Client;
+    tcp_arg(client_pcb, client);
+    tcp_sent(client_pcb, on_sent);
+    tcp_recv(client_pcb, on_recv);
+    tcp_err(client_pcb, on_err);
+
+    return send_response(*client, client_pcb);
+}
+
+void setup_http_server(u16_t port) {
+    debug("Starting server at %s on port %u\n", ip4addr_ntoa(netif_ip4_addr(netif_list)), port);
+
+    tcp_pcb *pcb = tcp_new_ip_type(IPADDR_TYPE_ANY);
+    if (!pcb) {
+        debug("failed to create server pcb\n");
         return;
     }
-    if (!tcp_server_open(state)) {
-        tcp_server_done(state);
+
+    err_t err = tcp_bind(pcb, IP_ADDR_ANY, port);
+    if (err) {
+        debug("failed to bind to port %u: %s\n", port, describe(err));
         return;
     }
-    while (!state->complete) {
-        async_context_poll(context);
-        async_context_wait_for_work_ms(context, 10 * 1000);
+
+    const u8_t backlog = 1;
+    pcb = tcp_listen_with_backlog_and_err(pcb, backlog, &err);
+    if (!pcb) {
+        debug("failed to listen: %s\n", describe(err));
+        return;
     }
-    free(state);
+
+    tcp_accept(pcb, on_accept);
 }
 
 int main() {
@@ -203,7 +212,7 @@ int main() {
     async_context_poll_t context = {};
     bool succeeded = async_context_poll_init_with_defaults(&context);
     if (!succeeded) {
-        printf("Failed to initialize async_context_poll_t\n");
+        debug("Failed to initialize async_context_poll_t\n");
         return -2;
     }
 
@@ -211,7 +220,7 @@ int main() {
 
     // 🇺🇸 🦅
     if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-        printf("failed to initialize WiFi\n");
+        debug("failed to initialize WiFi\n");
         return 1;
     }
 
@@ -225,15 +234,23 @@ int main() {
 
     cyw43_arch_enable_sta_mode();
 
-    printf("Connecting to WiFi...\n");
+    debug("Connecting to WiFi...\n");
     while (cyw43_arch_wifi_connect_timeout_ms("Annoying Saxophone", wifi_password, CYW43_AUTH_WPA2_AES_PSK, 20000)) {
-        printf("Failed to connect to WiFi. Trying again in 5 seconds.\n");
+        debug("Failed to connect to WiFi. Trying again in 5 seconds.\n");
         sleep_ms(5 * 1000);
     }
 
-    printf("Connected to WiFi.\n");
-    run_tcp_server_test(&context.core);
+    debug("Connected to WiFi.\n");
+    
+    setup_http_server(80);
 
+    debug("Entering event loop.\n");
+    for (;;) {
+      async_context_poll(&context.core);
+      async_context_wait_for_work_ms(&context.core, 10 * 1000);
+    }
+
+    // unreachable
     cyw43_arch_deinit();
     async_context_deinit(&context.core);
 }
