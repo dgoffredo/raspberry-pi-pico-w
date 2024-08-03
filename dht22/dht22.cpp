e6caa5fc189473d95b9ad0af31d5ed76a71d4bb7
David Goffredo <dmgoffredo@gmail.com>
2024-08-02 23:10:02 -0400

    document dht22.h

--- dht22/dht22.cpp
+++ dht22/dht22.cpp
@@ -1,91 +1,48 @@
-#include "dht22.h"
-
-#include <stdlib.h>
-#include <malloc.h>
-
-#include <chrono>
-
-#include <pico/async_context_poll.h>
-#include <pico/cyw43_arch.h>
-#include <pico/stdio.h>
-
 #include <picoro/coroutine.h>
+#include "dht22.h"
 #include <picoro/event_loop.h>
 #include <picoro/sleep.h>
 
+#include <pico/async_context_poll.h>
+#include <pico/stdio.h>
 
-uint32_t getTotalHeap() {
-   extern char __StackLimit, __bss_end__;
-   return &__StackLimit  - &__bss_end__;
-}
+#include <cassert>
+#include <chrono>
 
-uint32_t getFreeHeap() {
-   struct mallinfo m = mallinfo();
-   return getTotalHeap() - m.uordblks;
-}
-
-picoro::Coroutine<void> monitor_memory(async_context_t *context) {
-    for (int i = 0; ; ++i) {
-        const uint32_t bytes_free = getFreeHeap();
-        printf("%d: The heap has %lu bytes (%lu KB) free.\n", i, bytes_free, bytes_free / 1000UL);
-        co_await picoro::sleep_for(context, std::chrono::seconds(5));
+picoro::Coroutine<void> monitor_sensor(
+    async_context_t *ctx,
+    picoro::dht22::Driver *driver,
+    PIO pio,
+    uint8_t gpio_pin,
+    const char *name) {
+  picoro::dht22::Sensor sensor(driver, pio, gpio_pin);
+  for (;;) {
+    co_await picoro::sleep_for(ctx, std::chrono::seconds(2));
+    float celsius, humidity_percent;
+    const int rc = co_await sensor.measure(&celsius, &humidity_percent);
+    if (rc == 0) {
+      std::printf("%s: %.1f C, %.1f%% humidity\n", name, celsius,
+              humidity_percent);
     }
-}
-
-float celsius_to_fahrenheit(float temperature) {
-    return temperature * (9.0f / 5) + 32;
-}
-
-picoro::Coroutine<void> monitor_sensor(async_context_t *context, std::chrono::seconds initial_delay, picoro::dht22::Driver& driver, int gpio_pin) {
-    co_await picoro::sleep_for(context, initial_delay);
-    picoro::dht22::Sensor sensor(&driver, pio0, gpio_pin);
-    for (int i = 0; i < 10; ++i) {
-        // printf("%d: Before sleep\n", i);
-        co_await picoro::sleep_for(context, std::chrono::seconds(2));
-        // printf("%d: After sleep\n", i);
-        float celsius;
-        float humidity_percent;
-        // printf("%d: Before outer await\n", i);
-        const int rc = co_await sensor.measure(&celsius, &humidity_percent);
-        // printf("%d: After outer await\n", i);
-        if (rc) {
-            printf("gpio %d iteration %d: BAD THINGS BAD THINGS\n", gpio_pin, i);
-        } else {
-            printf("gpio %d iteration %d: %.1f C (%.1f F), %.1f%% humidity\n", gpio_pin, i, celsius, celsius_to_fahrenheit(celsius), humidity_percent);
-        }
-    }
+  }
 }
 
 int main() {
     stdio_init_all();
 
     async_context_poll_t context;
-    if (!async_context_poll_init_with_defaults(&context)) {
-        printf("Failed to initialize async_context_poll_t\n");
-        return -1;
-    }
-
-    // Do some WiFi chip setup here, just so that we can use the LED
-    // immediately. The rest of the setup happens in `networking`.
-    cyw43_arch_set_async_context(&context.core);
-    // 🇺🇸 🦅
-    /* TODO: no WiFi for now.
-    if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
-        printf("failed to initialize WiFi\n");
-        return -2;
-    }
-    */
+    const bool ok = async_context_poll_init_with_defaults(&context);
+    assert(ok);
+    async_context_t *const ctx = &context.core;
 
     constexpr int which_dma_irq = 0;
-    picoro::dht22::Driver driver(&context.core, which_dma_irq);
+    picoro::dht22::Driver driver(ctx, which_dma_irq);
 
-    picoro::run_event_loop(&context.core,
-        monitor_memory(&context.core),
-        monitor_sensor(&context.core, std::chrono::seconds(1), driver, 15),
-        monitor_sensor(&context.core, std::chrono::seconds(2), driver, 16),
-        monitor_sensor(&context.core, std::chrono::seconds(3), driver, 22));
+    picoro::run_event_loop(ctx,
+        monitor_sensor(ctx, &driver, pio0, 15, "top shelf"),
+        monitor_sensor(ctx, &driver, pio0, 16, "middle shelf"),
+        monitor_sensor(ctx, &driver, pio0, 22, "bottom shelf"));
 
     // unreachable
-    // TODO: no WiFi for now: cyw43_arch_deinit();
-    async_context_deinit(&context.core);
+    async_context_deinit(ctx);
 }
--- dht22/dht22.h
+++ dht22/dht22.h
@@ -1,5 +1,134 @@
 #pragma once
 
+// This file contains a driver for the [DHT22][1] temperature and humidity
+// sensor, which is also known as the [AM2302][2].
+//
+// The sensor can report the current temperature (in celsius) and relative
+// humidity (in percent) every two seconds. It communicates with the Pico using
+// a custom timing-based one-wire protocol.
+//
+// This driver uses PIO to negotiate the protocol timing. The driver supports
+// up to eight sensors: one per PIO state machine.
+//
+// The DMA interrupt handler and PIO programs are shared by multiple state
+// machines, so the driver consists of two classes: `class Driver` and
+// `class Sensor`.  Create one `Driver`, and then from it create one or more
+// `Sensor`s.
+//
+// `class Driver` sets up a (shared) DMA interrupt handler and is responsible
+// for loading and unloading the driver PIO program onto one or both of the PIO
+// devices, depending on the configuration of the `Sensor` objects created from
+// it.
+//
+// At most one instance of `Driver` may exist on a given core at a time, though
+// each core may have its own `Driver` instance. Do not share one `Driver`
+// instance between cores.
+//
+// A `Driver` instance must outlive any `Sensor` instances created from it.
+//
+// Do not scope the lifetime of `Driver` within a coroutine that might be
+// resumed by a `Sensor`'s DMA completion handler, as this could corrupt the
+// `async_context_t` event loop (one of the `Driver`'s data members is the
+// `async_when_pending_worker_t ` that handles DMA completion). It's best to
+// scope `Driver` to outlive the event loop (e.g. put it in `main()`).
+//
+// Instantiate `Driver` with an `async_context_t*` and a number indicating
+// which DMA IRQ to use (either `0` or `1`):
+//
+//     const uint8_t which_dma_irq = 0;
+//     Driver driver(&poll_context.core, which_dma_irq);
+//
+// Then you can create `Sensor` instances associated with the `Driver`. Each
+// sensor takes a PIO device and a GPIO pin, in addition to a pointer to the
+// `Driver`.
+//
+//     Sensor drawer(&driver, pio0, 15);
+//     Sensor middle_shelf(&driver, pio0, 16);
+//     Sensor top_shelf(&driver, pio0, 22);
+//     Sensor above(&driver, pio0, 4);
+//     Sensor behind(&driver, pio1, 5);
+//     Sensor beneath(&driver, pio1, 6);
+//
+// If a PIO device does not have any more available state machines, or if the
+// program cannot fit on the PIO device, then the `Sensor` constructor will
+// panic. It'll straight up flip out.
+//
+// Once instantiated, a `Sensor` can be used by a coroutine to make
+// measurements:
+//
+//     picoro::Coroutine<void> monitor_sensor(
+//         async_context_t *context, Sensor& sensor) {
+//       for (;;) {
+//         co_await picoro::sleep_for(std::chrono::seconds(2));
+//         float celsius, humidity_percent;
+//         const int rc = co_await sensor.measure(&celsius, &humidity_percent);
+//         if (rc == 0) {
+//           std::printf("%.1f C, %.1f%% humidity\n", celsius, humidity_percent);
+//         }
+//       }
+//     }
+//
+// `Sensor` has a member function
+// `measure(float *celsius, float *humidity_percent)` that a coroutine can
+// `co_await` to obtain an `int` error code. If the returned `int` is zero,
+// then the measurement succeeded; `*celsius` and `*humidity_percent` will
+// contain the current temperature and humidity, respectively. If the returned
+// `int` is nonzero, then the received data did not match its checksum.
+//
+// Here's a complete example:
+//
+//     #include <picoro/coroutine.h>
+//     #include <picoro/devices/dht22.h>
+//     #include <picoro/event_loop.h>
+//     #include <picoro/sleep.h>
+//
+//     #include <pico/async_context_poll.h>
+//     #include <pico/stdio.h>
+//
+//     #include <cassert>
+//     #include <chrono>
+//
+//     picoro::Coroutine<void> monitor_sensor(
+//         async_context_t *ctx,
+//         picoro::dht22::Driver *driver,
+//         PIO pio,
+//         uint8_t gpio_pin,
+//         const char *name) {
+//       picoro::dht22::Sensor sensor(driver, pio, gpio_pin);
+//       for (;;) {
+//         co_await picoro::sleep_for(ctx, std::chrono::seconds(2));
+//         float celsius, humidity_percent;
+//         const int rc = co_await sensor.measure(&celsius, &humidity_percent);
+//         if (rc == 0) {
+//           std::printf("%s: %.1f C, %.1f%% humidity\n", name, celsius,
+//                   humidity_percent);
+//         }
+//       }
+//     }
+//
+//     int main() {
+//         stdio_init_all();
+//
+//         async_context_poll_t context;
+//         const bool ok = async_context_poll_init_with_defaults(&context);
+//         assert(ok);
+//         async_context_t *const ctx = &context.core;
+//
+//         const int which_dma_irq = 0;
+//         picoro::dht22::Driver driver(ctx, which_dma_irq);
+//
+//         picoro::run_event_loop(ctx,
+//             monitor_sensor(ctx, &driver, pio0, 15, "top shelf"),
+//             monitor_sensor(ctx, &driver, pio0, 16, "middle shelf"),
+//             monitor_sensor(ctx, &driver, pio0, 22, "bottom shelf"));
+//
+//         // unreachable
+//         async_context_deinit(ctx);
+//     }
+//
+// [1]: https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf
+// [2]: https://files.seeedstudio.com/wiki/Grove-Temperature_and_Humidity_Sensor_Pro/res/AM2302-EN.pdf
+
 #include <algorithm>
 #include <array>
 #include <chrono>
@@ -199,6 +328,7 @@ Driver::~Driver() {
     const auto handler = which_dma_irq ? &Driver::dma_irq_handler<1> : &Driver::dma_irq_handler<0>;
     irq_remove_handler(irq, handler);
     if (!irq_previously_enabled) {
+        // TODO: but maybe somebody else has enabled it since...
         irq_set_enabled(irq, false);
     }
 
@@ -329,6 +459,8 @@ Coroutine<int> Sensor::measure(float *celsius, float *humidity_percent) {
         bool await_ready() {
             // If the transfer is done, then we don't need to suspend.
             // This assumes that the transfer has already begun.
+            // It's also very unlikely to be done, since we just poked the
+            // state machine with `pio_sm_put` above.
             return !dma_channel_is_busy(sensor->dma_channel);
         }
 
